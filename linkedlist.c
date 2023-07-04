1a146b802bd014ad6f83b9b725e7018e57d0f63f
RODEEKS <138195422+RODEEKS@users.noreply.github.com>
2023-07-04 15:55:33 +0530

    linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,3 +1,22 @@
+							LINKED LIST
+
+A linked list is a linear data structure that includes a series of connected nodes.
+Here, each node stores the data and the address of the next node.
+Types of Linked List - 
+1.Singly linked
+2.doubly linked and
+3.circular linked
+
+1.Singly Linked List : 
+It is the most common. Each node has data and a pointer to the next node.
+2.Doubly Linked List
+We add a pointer to the previous node in a doubly-linked list. Thus, we can go in either direction: forward or backward.
+3.Circular Linked List
+A circular linked list is a variation of a linked list in which the last element is linked to the first element. 
+This forms a circular loop.
+    
+SAMPLE CODE :-
+    
 #include<stdio.h>
 #include<stdlib.h>
 struct node
